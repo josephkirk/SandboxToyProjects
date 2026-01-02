adaad365c97b649ab770694a9b973516d2c61245
Nguyen Phi Hung <josephkirk.art@gmail.com>
2026-01-02 15:51:03 +0700

    cleanup odin

--- Odin/run_sim.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-pushd %~dp0
-
-echo ========================================
-echo Building Zig Client Simulator...
-echo ========================================
-pushd client_sim
-zig run client_sim/test_simulation.zig
